a87e1c74a28d65a040181dfb95a36f1c908e72a8
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2021-07-20 11:06:13 +0900

    updated md and tab content

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Homepage of Hasuzawa
 This is the homepage of Hasuzawa. This is not open-source but you are welcome to make suggestions.
+The website is written with React, JSX, Sass.
+For a structural overview of the website, please visit [the Wiki of this project](https://github.com/Hasuzawa/homepage/wiki/Overview).
 
 The brief outline of the website is given below.
 
@@ -7,8 +9,22 @@ The brief outline of the website is given below.
 
 
 ## Work Experience
+<details>
+  <summary>professional</summary>
+
+
+</details>
 
 ## Knowledge
+<details open>
+  <summary>more than a programmer</summary>
+
+  # Languages
+  -  English
+  -  Japanese
+  -  German
+  -  Chinese
+</details>
 
 ## Contact
 
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import navTab from "./Components/navTab.jsx";
+import navTab from "./components/navTab.jsx";
 import React from 'react';
 
 
@@ -17,9 +17,18 @@ function App() {
 class App extends React.Component {
   constructor(){
     super();
+    this.state = {}
+
 
 
     this.handleClick = this.handleClick.bind(this);
+  }
+  //I might want to keep track of the active tab by a state, and have a componentdidmount set the initial active state to the first page
+
+
+
+  displayContent(){
+    //hide all non-active tab content
   }
 
   handleClick(e){
@@ -32,11 +41,23 @@ class App extends React.Component {
     return (
       <div className = "App">
         <span>hello world</span>
-        <div>
-          <button id="1" onClick={this.handleClick}>Intro</button>
+
+        <navigator className = "tabMenu">
+          <button id={1} onClick={this.handleClick}>Intro</button>
           <button id={2} onClick={this.handleClick}>Experience</button>
           <button id={3} onClick={this.handleClick}>Contact</button>
+        </navigator>
+
+        <div id = "intro" className = "tabContent">
+          <h1>front page</h1>
+          <p>Welcome to my homepage.</p>
         </div>
+
+        <div>
+          <h1>second page</h1>
+          <p>This is the second page</p>
+        </div>
+
       </div>
     )
   }
