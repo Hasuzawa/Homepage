23fa860223d2ab1bd710fd53ac6c3e704a10d13e
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2021-08-14 21:45:30 +0900

    minor change, added tests

---
+++ src/Components/__tests__/Content.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { renderer } from "react-test-renderer";
+import Content, { TabIndex } from "../Content.jsx";
+
+/* test("page enum length=5", ()=>{
+    expect(TabIndex).toHaveLength(5);
+});
+
+
+test("render page 0",()=>{
+    const tree = renderer.create(<Content selectedTab={TabIndex[0]}/>);
+    expect(tree.props.displayTabContent()).toHaveReturned();
+}); */
